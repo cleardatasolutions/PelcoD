93840b1d761fb5b168a239b397a0fa82d7d9d146
Clear Data Solutions, Inc <info@cleardata.it>
2024-09-06 11:55:15 -0700

    Changed GetCommand to static function.  Added try/catch.  Updated README.md

--- pelcod.pas
+++ pelcod.pas
@@ -16,27 +16,32 @@ type
     const CMD_LEFT: TPacket = ($FF,$00,$00,$04,$00,$00,$00);
     const CMD_RIGHT: TPacket = ($FF,$00,$00,$02,$00,$00,$00);
   public
-    function GetCommand(var packet: TPacket; command: TPelcoCommand; data1: Byte; data2: Byte; address: Byte): Boolean;
+    class function GetCommand(var packet: TPacket; command: TPelcoCommand; data1: Byte; data2: Byte; address: Byte): Boolean; static;
 end;
 
 implementation
 
-function TPelcoD.GetCommand(var packet: TPacket; command: TPelcoCommand; data1: Byte; data2: Byte; address: Byte): Boolean;
+class function TPelcoD.GetCommand(var packet: TPacket; command: TPelcoCommand; data1: Byte; data2: Byte; address: Byte): Boolean; static;
 var
   checksum : Byte;
 begin
-  case command of
-    cmdGetPanPos: packet := CMD_GET_PAN_POS;
-    cmdLeft: packet := CMD_LEFT;
-    cmdRight: packet := CMD_RIGHT;
+  try
+    case command of
+      cmdGetPanPos: packet := CMD_GET_PAN_POS;
+      cmdLeft: packet := CMD_LEFT;
+      cmdRight: packet := CMD_RIGHT;
+    end;
+
+    packet[1] := address;
+    packet[4] := data1;
+    packet[5] := data2;
+
+    checksum := (packet[1]+packet[2]+packet[3]+packet[4]+packet[5]) Mod $100;
+    packet[6] := checksum;
+  except
+    result := false;
   end;
 
-  packet[1] := address;
-  packet[4] := data1;
-  packet[5] := data2;
-
-  checksum := (packet[1]+packet[2]+packet[3]+packet[4]+packet[5]) Mod $100;
-  packet[6] := checksum;
   result := true;
 end;
 
