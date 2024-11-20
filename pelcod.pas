ec936fa5fd1fdbdcabcee07a6a778cd294f84b40
Clear Data Solutions, Inc <info@cleardata.it>
2024-11-19 20:25:58 -0800

    .

--- pelcod.pas
+++ pelcod.pas
@@ -24,11 +24,39 @@ type TPelcoCommand = (
 
 type TMovements = set of TPelcoCommand;
 
-  {TPelcoResponse }
+  { TPanDirection }
+
+type TPanDirection = (
+  pdRight,        {Pan Right}
+  pdLeft          {Pan Left}
+);
+
+type TTiltDirection = (
+  tdUp            {Tilt Up},
+  tdDown          {Tilt Down}
+);
+
+  { TPelcoResponse }
 
 type TPelcoResponse = (
   rspPanPos=$59,  {Pan Position Response}
   rspTiltPos=$5B  {Tilt Position Response}
+);
+
+  { TPelcoSpeed }
+
+type TPelcoSpeed = (
+  spdStop=$00, {Stop Pan/Tilt}
+  spd01=$01, spd02=$02, spd03=$03, spd04=$04, spd05=$05, spd06=$06, spd07=$07,
+  spd08=$08, spd09=$09, spd10=$0a, spd11=$0b, spd12=$0c, spd13=$0d, spd14=$0e,
+  spd15=$0f, spd16=$10, spd17=$11, spd18=$12, spd19=$13, spd20=$14, spd21=$15,
+  spd22=$16, spd23=$17, spd24=$18, spd25=$19, spd26=$1a, spd27=$1b, spd28=$1c,
+  spd29=$1d, spd30=$1e, spd31=$1f, spd32=$20, spd33=$21, spd34=$22, spd35=$23,
+  spd36=$24, spd37=$25, spd38=$26, spd39=$27, spd40=$28, spd41=$29, spd42=$2a,
+  spd43=$2b, spd44=$2c, spd45=$2d, spd46=$2e, spd47=$2f, spd48=$30, spd49=$31,
+  spd50=$32, spd51=$33, spd52=$34, spd53=$35, spd54=$36, spd55=$37, spd56=$38,
+  spd57=$39, spd58=$3a, spd59=$3b, spd60=$3c, spd61=$3d, spd62=$3e, spd63=$3f,
+  spdMax=$FF {Turbo}
 );
 
   { TPacket }
